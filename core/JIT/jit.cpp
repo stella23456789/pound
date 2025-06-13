8bdd9468de89559de1bffdfc1891c28a217cbe7c
SamsonLib <Samson.gw21@gmail.com>
2025-06-14 01:29:16 +0200

    Added Makefile and made things run on my machine (im sorry ); )

--- core/JIT/jit.cpp
+++ core/JIT/jit.cpp
@@ -1,18 +1,20 @@
-#include "core/JIT/jit.h"
+#include "jit.h"
 #include <sys/mman.h>
 #include <cstring>
 #include <cstdio>
 #include <vector>
 
-using JitFunc = void(*)();
+using JitFunc = void (*)();
 
-void JIT::translate_and_run(CPU& cpu) {
-    uint8_t* code = (uint8_t*)mmap(nullptr, 64, PROT_READ | PROT_WRITE | PROT_EXEC,
-                                   MAP_PRIVATE | MAP_ANON, -1, 0);
+void JIT::translate_and_run(CPU &cpu)
+{
+    uint8_t *code = (uint8_t *)mmap(nullptr, 64, PROT_READ | PROT_WRITE | PROT_EXEC,
+                                    MAP_PRIVATE | MAP_ANON, -1, 0);
     size_t offset = 0;
 
     // Decode mock instructions from cpu.memory
-    if (cpu.memory[0] == 0x05) { // MOVZ placeholder
+    if (cpu.memory[0] == 0x05)
+    {                          // MOVZ placeholder
         code[offset++] = 0x48; // mov rax, imm64
         code[offset++] = 0xB8;
         uint64_t imm = 5;
@@ -20,7 +22,8 @@ void JIT::translate_and_run(CPU& cpu) {
         offset += 8;
     }
 
-    if (cpu.memory[4] == 0x03) { // ADD placeholder
+    if (cpu.memory[4] == 0x03)
+    {                          // ADD placeholder
         code[offset++] = 0x48; // add rax, imm32
         code[offset++] = 0x05;
         uint32_t addval = 3;
@@ -32,13 +35,12 @@ void JIT::translate_and_run(CPU& cpu) {
 
     JitFunc fn = reinterpret_cast<JitFunc>(code);
     uint64_t result;
-    asm volatile (
+    asm volatile(
         "call *%1\n"
         "mov %%rax, %0\n"
         : "=r"(result)
         : "r"(fn)
-        : "%rax"
-    );
+        : "%rax");
 
     cpu.regs[0] = result;
 }
