677893b4b76a0380da89aa01f0a781f965e77e58
Xphalnos <yiga.steam@gmail.com>
2025-06-20 21:14:51 +0200

    SDL3 Basic Window

--- core/main.cpp
+++ core/main.cpp
@@ -2,8 +2,35 @@
 
 #include "Base/Logging/Backend.h"
 
+#include <SDL3/SDL.h>
+
 #include "ARM/cpu.h"
 #include "JIT/jit.h"
+
+SDL_Window *Window{};
+SDL_Event windowEvent;
+
+void initSDL3() {
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        LOG_ERROR(Render, "Error while creating SDL3 Context!");
+    }
+
+    SDL_PropertiesID props = SDL_CreateProperties();
+    SDL_SetStringProperty(props, SDL_PROP_WINDOW_CREATE_TITLE_STRING, "Pound Emulator");
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_X_NUMBER, SDL_WINDOWPOS_CENTERED);
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_Y_NUMBER, SDL_WINDOWPOS_CENTERED);
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_WIDTH_NUMBER, /*Config::windowWidth()*/ 800);
+    SDL_SetNumberProperty(props, SDL_PROP_WINDOW_CREATE_HEIGHT_NUMBER, /*Config::windowHeight()*/ 600);
+    // For a new Vulkan support, don't forget to change 'SDL_WINDOW_OPENGL' by 'SDL_WINDOW_VULKAN'.
+    SDL_SetNumberProperty(props, "flags", SDL_WINDOW_OPENGL);
+    SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_RESIZABLE_BOOLEAN, true);
+    SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, true);
+    Window = SDL_CreateWindowWithProperties(props);
+    SDL_DestroyProperties(props);
+
+    SDL_SetWindowMinimumSize(Window, 640, 480);
+}
 
 int main() {
 
@@ -12,6 +39,8 @@ int main() {
 
     const auto config_dir = Base::FS::GetUserPath(Base::FS::PathType::BinaryDir);
     Config::Load(config_dir / "config.toml");
+
+    initSDL3();
 
     CPU cpu;
     cpu.pc = 0;
@@ -30,6 +59,23 @@ int main() {
     cpu.print_debug_information();
 
     LOG_INFO(ARM, "X0 = {}", cpu.x(0));
+
+    bool rendering = true;
+
+    while (rendering) {
+        // Process events.
+        while (SDL_PollEvent(&windowEvent)) {
+            switch (windowEvent.type) {
+                case SDL_EVENT_QUIT:
+                SDL_DestroyWindow(Window);
+                SDL_Quit();
+                rendering = false;
+            break;
+            default:
+            break;
+            }
+        }
+    }
 
     return 0;
 }
