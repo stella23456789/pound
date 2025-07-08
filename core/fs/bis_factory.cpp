4b4e858dbcd59cde5273adb797e464f8b4864b49
ownedbywuigi <ownedbywuigi@gmail.com>
2025-07-08 05:28:47 +0100

    3rd_Party jumpscare

--- core/fs/bis_factory.cpp
+++ core/fs/bis_factory.cpp
@@ -3,7 +3,7 @@
 
 // Modified by Pound emulator for SW2 compatibility
 
-#include </fmt/include/fmt/format.h>
+#include </3rd_Party/fmt/include/fmt/format.h>
 #include "core/common/fs/path_util.h"
 #include "core/fs/bis_factory.h"
 #include "core/fs/registered_cache.h"
