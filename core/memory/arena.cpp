7897d0e2a28ca6e72d91af586030a696e95cec27
Ronald Caesar <github43132@proton.me>
2025-07-09 17:56:39 -0400

    fix(memory): remove the inclusion of mman.h on windows.
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- core/memory/arena.cpp
+++ core/memory/arena.cpp
@@ -7,7 +7,8 @@
 Memory::Arena Memory::arena_init() {
     // TODO(GloriousEggroll): Replace malloc with a windows memory mapping API.
 #ifdef WIN32
-    static_cast<uint8_t*>(malloc(sizeof(uint8_t) * MEMORY_CAPACITY));
+    auto data =
+        static_cast<uint8_t*>(malloc(sizeof(uint8_t) * MEMORY_CAPACITY));
 #else
     void* data = mmap(nullptr, MEMORY_CAPACITY, PROT_READ | PROT_WRITE,
                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
