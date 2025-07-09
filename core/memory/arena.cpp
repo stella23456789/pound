d1ada1740aa896043d3679e3335013cc87705a00
Ronald Caesar <github43132@proton.me>
2025-07-09 17:38:10 -0400

    fix(memory): Fixed missing mman.h include definitions
    This commit removes a custom mmap() function which was supposed
    to support both windows and linux but the linux and macOS builds
    failed to compile.
    
    The custom mmap() function will be replaced by malloc() for windows
    and linux's native mmap().
    
    Signed-off-by: Ronald Caesar <github43132@proton.me>

--- core/memory/arena.cpp
+++ core/memory/arena.cpp
@@ -1,24 +1,17 @@
 #include "arena.h"
+#include <sys/mman.h>
 #include "Base/Assert.h"
-#include "sys/mman.h"
-
-// Memory::Arena Memory::arena_init() {
-//     // TODO(GloriousTaco): The line below is stupidly unsafe. Replace malloc with mmap() and check the return value.
-//     auto data =
-//         static_cast<uint8_t*>(malloc(sizeof(uint8_t) * MEMORY_CAPACITY));
-//     Memory::Arena arena = {
-//         .capacity = MEMORY_CAPACITY,
-//         .size = 0,
-//         .data = data,
-//     };
-//     return arena;
-// }
-// old unsafe code
 
 Memory::Arena Memory::arena_init() {
-    void* data = mmap(nullptr, MEMORY_CAPACITY, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    // TODO(GloriousEggroll): Replace malloc with a windows memory mapping API.
+#ifdef WIN32
+    static_cast<uint8_t*>(malloc(sizeof(uint8_t) * MEMORY_CAPACITY));
+#else
+    void* data = mmap(nullptr, MEMORY_CAPACITY, PROT_READ | PROT_WRITE,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
     if (data == MAP_FAILED) {
-        return {0, 0, nullptr}; // Return invalid arena on failure
+        return {0, 0, nullptr};  // Return invalid arena on failure
     }
     Memory::Arena arena = {
         .capacity = MEMORY_CAPACITY,
